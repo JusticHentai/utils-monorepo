f8057b0d9d54bd93f322216f5ab970181dc5d9eb
justichentai <1360636180@qq.com>
2024-08-31 22:32:20 +0800

    feat: all

---
+++ debug/src/components/components.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@justichentai/components'
+
+function Components() {
+  console.log(Button)
+
+  return (
+    <>
+      <Button test={true} />
+    </>
+  )
+}
+
+export default Components
