79b3b90f779b4105b7a2cbc4d4971d87382df914
justichentai <1360636180@qq.com>
2024-12-21 20:01:06 +0800

    fix: 普通更新

--- debug/src/App.tsx
+++ debug/src/App.tsx
@@ -1,11 +1,13 @@
 import Components from './components/components'
 import Style from './components/style'
+import Template from './components/template'
 
 function App() {
   return (
     <>
       <Components />
       <Style />
+      <Template />
     </>
   )
 }
---
+++ debug/src/components/template.tsx
@@ -0,0 +1,14 @@
+import Utils from '@justichentai/utils-template'
+
+const Components = () => {
+  const template = new Utils({})
+  console.log(template)
+
+  return (
+    <>
+      <div />
+    </>
+  )
+}
+
+export default Components
