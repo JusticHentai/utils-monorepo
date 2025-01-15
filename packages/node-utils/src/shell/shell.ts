d3682ec22abfd3887be2415962912d3ea1281e58
justichentai <1360636180@qq.com>
2025-01-16 00:18:56 +0800

    test

--- packages/node-utils/src/shell/shell.ts
+++ packages/node-utils/src/shell/shell.ts
@@ -1,4 +1,3 @@
-import Loading from '@/logger/loading'
 import logger from '@/logger/logger'
 import { exec } from 'child_process'
 
@@ -6,17 +5,17 @@ export type Result = [string, undefined] | [undefined, string]
 
 const shellExec = (command: string): Promise<Result> => {
   return new Promise((resolve) => {
-    const loading = new Loading()
-    loading.start(`执行：${command}`)
+    // const loading = new Loading()
+    // loading.start(`执行：${command}`)
 
     const commandExec = exec(command, (error, stdout, stderr) => {
       if (error) {
-        loading.fail(`执行失败：${command}`)
+        // loading.fail(`执行失败：${command}`)
         logger.error('[stderr]: ', stderr)
 
         resolve([undefined, stderr])
       } else {
-        loading.succeed(`执行成功：${command}`)
+        // loading.succeed(`执行成功：${command}`)
         logger.info('[stdout]: ', stdout)
 
         resolve([stdout, undefined])
