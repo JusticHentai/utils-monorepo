f7bef7e448bcdd4db388e205bc375db53653544c
justichentai <1360636180@qq.com>
2025-01-24 02:00:03 +0800

    普通更新

--- packages/js-utils/src/surveillance/constants.ts
+++
@@ -1,9 +0,0 @@
-export const DEFAULT_INFO = `[surveillance][eventInit]`
-
-export enum EVENT {
-  DOM_CONTENT_LOADED = 'DOMContentLoaded',
-  LOAD = 'load',
-  BEFORE_UNLOADED = 'beforeUnload',
-  UNLOAD = 'unload',
-  SSR = 'ssr',
-}
--- packages/js-utils/src/surveillance/types.ts
+++
@@ -1,1 +0,0 @@
-export type CallBack = (e?: Event) => any
