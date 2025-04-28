5622fa64486b23c98d66bc5de815c68750690d00
justichentai <1360636180@qq.com>
2025-04-28 22:18:29 +0800

    普通更新

--- packages/js-utils/src/index.ts
+++ packages/js-utils/src/index.ts
@@ -3,6 +3,7 @@ export { default as awaitFormat } from './async/awaitFormat'
 export { default as ResourceController } from './async/resource-controller'
 export { default as retry } from './async/retry'
 export { default as sleep } from './async/sleep'
+export { default as timeout } from './async/timeout'
 
 // error
 export { default as collectError } from './error'
@@ -28,11 +29,14 @@ export { default as Point } from './math/axis/Point'
 export { default as random } from './math/random'
 
 // object
-export { default as deepMerge } from './object/deepMerge'
+export { default as merge } from './object/merge'
 export { default as pick } from './object/pick'
 export { default as walk } from './object/walk'
 
 // time
+export { default as countdown } from './time/countdown'
+export { default as formatTime } from './time/formatTime'
 export { default as getLocalTime } from './time/getLocalTime'
+export { default as getTimezone } from './time/getTimezone'
 export { default as Timer } from './time/Timer'
 export { default as timeTransform } from './time/timeTransform'
